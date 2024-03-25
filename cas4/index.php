b8649ae67b31e0c03bd274d2d6c5e58ae989c01d
Kima018 <marijan.dev@gmail.com>
2024-03-25 21:34:12 +0100

    cas4 done

--- cas4/index.php
+++ cas4/index.php
@@ -12,9 +12,14 @@ function checkUser(string $name, string $password): string
     }
 }
 
-?>
+$current_time = date("H");
+$part_of_the_day = match (true) {
+    $current_time >= 5 && $current_time < 12 => "jutro",
+    $current_time >= 12 && $current_time < 20 => "popodne",
+    default => "noc",
+}; ?>
 <!doctype html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -22,12 +27,16 @@ function checkUser(string $name, string $password): string
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
+<br>
 <form action="" method="POST">
+    <h2>Unesi podatke admine</h2>
     <input type="text" name="name" placeholder="enter username"/>
     <input type="password" name="password" placeholder="enter password"/>
     <input type="submit" value="Submit"/>
 </form>
+<br>
+
 <h1><?= $output ?></h1>
+<h3>Trenutno je <?= $part_of_the_day ?> </h3>
 </body>
 </html>
