d16765123dc410d4375c617d035c63016d068cf9
Kima018 <marijan.dev@gmail.com>
2024-03-26 09:43:42 +0100

    cas4 GET domaci zavrsen

--- cas5GET/index.php
+++ cas5GET/index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
+<h1>Ex 1</h1>
 <form method="get" action="calculate.php">
     <input type="number" name="num_1">
     <input type="number" name="num_2">
@@ -15,8 +16,17 @@
         <option value="addition" selected>add</option>
         <option value="subtraction">subtract</option>
     </select>
-    <input type="submit" value="Submit" >
+    <input type="submit" value="Submit">
 </form>
-
+<h1>Ex 2/HW</h1>
+<form method="get" action="product.php">
+    <input type="number" name="price">
+    <select name="type">
+        <option value="food" selected>Food</option>
+        <option value="pc_components">Pc components</option>
+    </select>
+    <input type="checkbox" name="add_pdv" value="PDV">
+    <input type="submit" value="Submit">
+</form>
 </body>
 </html>
--- cas5GET/product.php
+++ cas5GET/product.php
@@ -1,1 +1,31 @@
 <?php
+ini_set('intl.default_locale', 'en_US');
+$final_price = 0;
+$entry_price =intval($_GET['price']);
+$type_of_product=$_GET['type'];
+if ($type_of_product==="food"){
+    $final_price=$entry_price + 150;
+}elseif ($type_of_product==="pc_components"){
+    $final_price=$entry_price + 350;
+}
+if (isset($_GET['add_pdv'])){
+    $final_price+=$final_price*0.2;
+}
+//$locale = 'en_US';
+//$fmt = new NumberFormatter($locale, NumberFormatter::CURRENCY);
+//$formatted_price = $fmt->formatCurrency($final_price, 'USD');
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1><?= $final_price ?>$</h1>
+</body>
+</html>
