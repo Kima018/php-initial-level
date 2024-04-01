2133532a9db151e1c6cc5e9ed02a3dd3cbebb561
Kima018 <marijan.dev@gmail.com>
2024-04-01 23:26:27 +0200

    cas10 dodat

--- cas9/index.php
+++ cas9/index.php
@@ -45,18 +45,18 @@ function izdracunajDostavu($city)
         "3" => 500,
     ];
 
-    foreach ($dostava as $grad => $razdaljina){
-        if ($razdaljina<=100) return $cenaDostave['1']=200;
-        if ($razdaljina<=200) return $cenaDostave['2']=200;
-        if ($razdaljina<=100) return $cenaDostave['1']=200;
-    }
-
-    if ($city == "Subotica") {
-        return "Rastojanje Beograd - " . $city . "je" . $dostava['Subotica'] ."km, dostava je " . $cenaDostave['1'] ;
-    }
-
-
-}
+//    foreach ($dostava as $grad => $razdaljina){
+//        if ($razdaljina<=100) return $cenaDostave['1']=200;
+//        if ($razdaljina<=200) return $cenaDostave['2']=200;
+//        if ($razdaljina<=100) return $cenaDostave['1']=200;
+//    }
+//
+//    if ($city == "Subotica") {
+//        return "Rastojanje Beograd - " . $city . "je" . $dostava['Subotica'] ."km, dostava je " . $cenaDostave['1'] ;
+//    }
+//
+//
+//}
 
 
 ?>
