f62978fa5ed69b36d434369cbc7b689c31eb6f03
Kima018 <marijan.dev@gmail.com>
2024-04-02 09:55:34 +0200

    cas10, domaci odradjen

--- cas10/index.php
+++ cas10/index.php
@@ -1,29 +1,50 @@
 <?php
 
-function izdracunajDostavu($city)
+function calculateDelivery(int $price, string $city)
 {
-    $dostava = [
+    $delivery = [
         "Subotica" => 220,
         "Pancevo" => 10,
         "Sarajevo" => 292,
         "Split" => 799
     ];
 
-    $cenaDostave = [
+    $priceList = [
         "1" => 200,
         "2" => 350,
         "3" => 500,
     ];
+    
+    if (!array_key_exists($city, $delivery)) die("Grad ne postoji");
+    $deliveryPrice = $priceList['1'];
+    if ($delivery[$city] <= 200 && $delivery[$city] > 100) $deliveryPrice = $priceList['2'];
+    if ($delivery[$city] > 200) $deliveryPrice = $priceList['3'];
 
-    foreach ($dostava as $grad => $razdaljina){
-        if ($razdaljina<=100) return $cenaDostave['1']=200;
-        if ($razdaljina<=200) return $cenaDostave['2']=200;
-        if ($razdaljina<=100) return $cenaDostave['1']=200;
-    }
-
-    if ($city == "Subotica") {
-        return "Rastojanje Beograd - " . $city . "je" . $dostava['Subotica'] ."km, dostava je " . $cenaDostave['1'] ;
-    }
-
+    return [
+        "city" => $city,
+        "km" => $delivery[$city],
+        "deliveryPrice" => $deliveryPrice,
+        "totalPrice" => $price + $deliveryPrice
+    ];
 
 }
+
+$delivery = calculateDelivery(2000, "Subotica");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Rastojanje Beograd - <?= $delivery['city'] ?> je <?= $delivery['km'] ?>km i cena dostave
+    je <?= $delivery['deliveryPrice'] ?> din.</h1>
+<h2>Ukupna cena sa dostavom iznosi <?= $delivery['totalPrice'] ?> din.</h2>
+</body>
+</html>
+
